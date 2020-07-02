266d58e88d72f57401d17d0377233ba892bb0694
mona mohamed <monaelhoby@yahoo.com>
2020-07-02 18:21:25 +0200

    Updates

---
+++ precache-manifest.09e20fdec4cedc68dec61f77f794f965.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5960e5f265887ab2531b38234e8b1b99",
+    "url": "/React_Blogger_app/index.html"
+  },
+  {
+    "revision": "de610b525f2930db39a0",
+    "url": "/React_Blogger_app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f367e27be3c020697d6",
+    "url": "/React_Blogger_app/static/js/2.cd648cda.chunk.js"
+  },
+  {
+    "revision": "f48955458710786629c9fab78abba7c6",
+    "url": "/React_Blogger_app/static/js/2.cd648cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de610b525f2930db39a0",
+    "url": "/React_Blogger_app/static/js/main.0d3fdc63.chunk.js"
+  },
+  {
+    "revision": "189d8e21ee4d69ea31c5",
+    "url": "/React_Blogger_app/static/js/runtime-main.328c7af3.js"
+  },
+  {
+    "revision": "4bf24c61af8a82e121dedf51e5250fec",
+    "url": "/React_Blogger_app/static/media/bkg.4bf24c61.jpg"
+  }
+]);
